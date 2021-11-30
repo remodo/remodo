d19d09afc90c348dfad3d7e87c0b741b6558bdf2
remodo <remodo@msn.com>
2021-11-30 09:26:53 -0800

    All working: separation of JS and CSS from HTML, blinking title, day of week script

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-function display_ct7() {
-var d = new Date()
-var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
-hours = x.getHours( ) % 12;
-hours = hours ? hours : 12;
-hours=hours.toString().length==1? 0+hours.toString() : hours;
-}
+const d = new Date();
 
-var minutes=x.getMinutes().toString()
-minutes=minutes.length==1 ? 0+minutes : minutes;
-
-var seconds=x.getSeconds().toString()
-seconds=seconds.length==1 ? 0+seconds : seconds;
-
-var monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var dayArr = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
 
 let dayOfWeek = dayArr[d.getDay()];
 document.getElementById("dayOfWeek").innerHTML = "Today is " + dayOfWeek;
-
-function display_c7(){
-    var refresh=1000; // Refresh rate in milli seconds
-    mytime=setTimeout('display_ct7()',refresh)
-}
