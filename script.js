0b77394271bf2dcf5a6d4c5810c773e7f0c03e88
remodo <remodo@msn.com>
2021-11-30 15:48:46 -0800

    Working blinking title, day and time with seconds JS scripts, and acceptable CSS styles

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const d = new Date();
+function displayPage() {
+const now = new Date();
 
 var dayArr = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-let dayOfWeek = dayArr[d.getDay()];
-document.getElementById("dayOfWeek").innerHTML = "Today is " + dayOfWeek;
+let dayOfWeek = dayArr[now.getDay()];
+document.getElementById("dayOfWeek").innerHTML = dayOfWeek;
+
+let ampm = now.getHours() >= 12 ? ' PM' : ' AM';
+let hr = now.getHours() % 12;
+let min = now.getMinutes();
+//let min = min.toString().length == 1 ? 0 + min.toString() : min;
+let sec = now.getSeconds();
+//let sec = sec.toString().length == 1 ? 0 + sec.toString() : sec;
+let time12 = hr + ":" + min + ":" + sec + " " + ampm;
+document.getElementById("time12").innerHTML = time12;
+refreshPage();
+}
+
+function refreshPage(){
+    var refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('displayPage()',refresh)
+}
+    
